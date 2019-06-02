00ea78db485023704ad962c86c3b5c0471d0fa68
Behrang N <behrangno@gmail.com>
2019-06-02 17:43:40 +0430

    Add request validation for email sending

---
+++ app/Http/Requests/SendEmailPost.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class SendEmailPost extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'subject' => 'required|string',
+            'from' => 'email',
+            'fromName' => 'string',
+            'to' => 'required|email',
+            'toName' => 'string',
+            'contentType' => 'required|string',
+            'content' => 'required'
+        ];
+    }
+}
