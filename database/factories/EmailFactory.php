d998463909003954edb02909f19ad9d446f7cccd
Behrang N <behrangno@gmail.com>
2019-06-02 18:15:53 +0430

    Add email's model and factory

---
+++ database/factories/EmailFactory.php
@@ -0,0 +1,51 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+use App\Models\Email;
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(Email::class, function (Faker $faker) {
+
+    /*
+     * Create receive email data
+     */
+    $data = [
+        'subject' => $faker->sentence,
+        'from' => $faker->email,
+        'fromName' => $faker->name,
+        'to' => $faker->email,
+        'toName' => $faker->name,
+        'contentType' => $faker->randomElement(['text/html', 'text/string'])
+    ];
+
+    // Add content based on contentType
+    if ($data['contentType'] == 'text/html') {
+
+        $data['content'] = $faker->randomHtml(1,1);
+
+    } elseif ($data['contentType'] == 'text/string') {
+
+        $data['content'] = $faker->paragraph;
+
+    }
+
+    // Convert data to json
+    $data = json_encode($data);
+
+    return [
+        'status' => $faker->numberBetween(0, 3),
+        'data' => $data,
+        'received_at' => now(),
+    ];
+});
