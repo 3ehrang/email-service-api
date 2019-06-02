5a36183be683e2ca66a98f6f0990cf425b2168b7
Behrang N <behrangno@gmail.com>
2019-06-02 20:06:18 +0430

    Add a test for checking database connection lost

--- tests/Feature/EmailService/Api/V1/EmailSendTest.php
+++ tests/Feature/EmailService/Api/V1/EmailSendTest.php
@@ -6,10 +6,42 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\DatabaseMigrations;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use App\Models\Email;
+use Illuminate\Support\Facades\Schema;
 
 class EmailSendTest extends TestCase
 {
     use DatabaseMigrations, RefreshDatabase;
+
+    /**
+     * Test if database connection with emails table lost
+     *
+     * @test
+     */
+    public function can_not_save_email_into_database()
+    {
+        // Drop emails table
+        Schema::dropIfExists('emails');
+
+        // Get email's sample data
+        $email = $this->getSampleData();
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Assert it was successful and response was acceptable
+        $response
+            ->assertStatus(422)
+            ->assertJsonStructure([
+                'status',
+                'message',
+                'data'
+            ])
+            ->assertJson([
+                'status' => 'fail',
+                'message' => 'Unable to communicate with database.'
+            ]);
+    }
+
     /**
      * Test send email end-point
      *
