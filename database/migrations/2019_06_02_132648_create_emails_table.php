916aefca2cab1330d180d9ae3651c11253afde9e
Behrang N <behrangno@gmail.com>
2019-06-02 18:14:53 +0430

    Add migration for emails table

---
+++ database/migrations/2019_06_02_132648_create_emails_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateEmailsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emails', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table
+                ->unsignedTinyInteger('status')
+                ->default(0)
+                ->comment('0 received, 1 queued, 2 sent, 3 failed')
+            ;
+            $table->json('data')->comment('email received json data');
+            $table->timestamp('received_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emails');
+    }
+}
