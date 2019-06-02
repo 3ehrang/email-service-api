8ade51bb3129bf4b17245d3e43739ca71e248274
Behrang N <behrangno@gmail.com>
2019-06-02 17:43:11 +0430

    Add test for email request validation

---
+++ tests/Feature/EmailService/Api/V1/EmailSendValidationTest.php
@@ -0,0 +1,207 @@
+<?php
+
+namespace Tests\Feature\Api\EmailService\V1;
+
+use Tests\TestCase;
+use Illuminate\Foundation\Testing\WithFaker;
+
+class EmailSendValidationTest extends TestCase
+{
+    use WithFaker;
+
+    /**
+     * Email must has subject
+     *
+     * @test
+     */
+    public function email_without_subject_is_not_valid()
+    {
+        // Get sample data and remove subject
+        $email = $this->getSampleData();
+        unset($email['subject']);
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Check failure response is correct
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('subject');
+    }
+
+    /**
+     * Email must has receiver mail address
+     *
+     * @test
+     */
+    public function email_without_receiver_address_is_not_valid()
+    {
+        // Get sample data and remove receiver address
+        $email = $this->getSampleData();
+        unset($email['to']);
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Check failure response is correct
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('to');
+    }
+
+    /**
+     * Email must has contentType
+     *
+     * @test
+     */
+    public function email_without_content_type_is_not_valid()
+    {
+        // Get sample data and remove contentType
+        $email = $this->getSampleData();
+        unset($email['contentType']);
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Check failure response is correct
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('contentType');
+    }
+
+    /**
+     * Email must has content
+     *
+     * @test
+     */
+    public function email_without_content_is_not_valid()
+    {
+        // Get sample data and remove content
+        $email = $this->getSampleData();
+        unset($email['content']);
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Check failure response is correct
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('content');
+    }
+
+    /**
+     * @test
+     */
+    public function email_without_to_name_is_valid()
+    {
+        // Get sample data and remove content
+        $email = $this->getSampleData();
+        unset($email['toName']);
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Assert it was successful and response was acceptable
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'status',
+                'message',
+                'data'
+            ])
+            ->assertJson([
+                'status' => 'success',
+                'message' => 'Queued. Thank you.',
+            ]);
+    }
+
+    /**
+     * @test
+     */
+    public function email_without_from_name_is_valid()
+    {
+        // Get sample data and remove content
+        $email = $this->getSampleData();
+        unset($email['fromName']);
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Assert it was successful and response was acceptable
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([
+                'status',
+                'message',
+                'data'
+            ])
+            ->assertJson([
+                'status' => 'success',
+                'message' => 'Queued. Thank you.',
+            ]);
+    }
+
+    /**
+     * Email's receiver address must be a valid email address
+     *
+     * @test
+     */
+    public function receiver_email_format_muse_be_valid()
+    {
+        // Get sample data
+        $email = $this->getSampleData();
+        $email['to'] = 'example.co';
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Check failure response is correct
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('to');
+    }
+
+    /**
+     * Email's receiver address must be a valid email address if provide
+     *
+     * @test
+     */
+    public function sender_email_format_muse_be_valid()
+    {
+        // Get sample data
+        $email = $this->getSampleData();
+        $email['from'] = 'example.co';
+
+        // Send email request
+        $response = $this->json('POST', route('email.service.api.v1.email.send'), $email);
+
+        // Check failure response is correct
+        $response->assertStatus(422);
+        $response->assertJsonValidationErrors('from');
+    }
+
+    /**
+     * Provide simple sending email data
+     *
+     * @return array
+     */
+    public function getSampleData()
+    {
+        $data = [
+            'subject' => $this->faker->sentence,
+            'from' => $this->faker->email,
+            'fromName' => $this->faker->name,
+            'to' => $this->faker->email,
+            'toName' => $this->faker->name,
+            'contentType' => $this->faker->randomElement(['text/html', 'text/string'])
+        ];
+
+        // Add content based on contentType
+        if ($data['contentType'] == 'text/html') {
+
+            $data['content'] = $this->faker->randomHtml(1,1);
+
+        } elseif ($data['contentType'] == 'text/string') {
+
+            $data['content'] = $this->faker->paragraph;
+
+        }
+
+        return $data;
+    }
+}
