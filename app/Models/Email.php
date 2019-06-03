69a9ed25af873208bd0599b7e98b5d7a4ccd02f5
Behrang N <behrangno@gmail.com>
2019-06-03 16:43:19 +0430

    Add mutator to email model for 'received_at' and 'data' column

--- app/Models/Email.php
+++ app/Models/Email.php
@@ -14,4 +14,28 @@ class Email extends Model
     protected $fillable = [
         'sid', 'status', 'data', 'receive_at',
     ];
+
+    /**
+     * Set the email's received at.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setReceivedAtAttribute($value)
+    {
+        if (!$value) {
+            $this->attributes['received_at'] = now();
+        }
+    }
+
+    /**
+     * Set the email's received at.
+     *
+     * @param  string  $value
+     * @return void
+     */
+    public function setDataAttribute($value)
+    {
+        $this->attributes['data'] = json_encode($value);
+    }
 }
