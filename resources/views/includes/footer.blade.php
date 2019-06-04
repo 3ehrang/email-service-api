851592ed948e647b1a45492ad27dce1001d6996c
Behrang N <behrangno@gmail.com>
2019-06-04 12:28:53 +0430

    Add home page content

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,26 @@
+<div class="container">
+    <div class="row mb-4">
+        <a class="navbar-brand col-12" href="#">
+            <img src="" width="171" height="55" class="d-inline-block align-top" alt="Chance Pack">
+        </a>
+    </div>
+    <div class="row">
+        <div class="col-12 col-md-4">
+            <h4>About Us</h4>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aspernatur consequatur corporis, doloribus dolorum eum ex illum laboriosam minima necessitatibus nobis nostrum, numquam obcaecati pariatur quae quibusdam sit soluta totam voluptas.</p>
+        </div>
+        <div class="col-6 col-md-4 text-left text-md-center">
+            <h4>Site map</h4>
+            <ul class="list-unstyled">
+                <li><a href="/">Welcome</a></li>
+            </ul>
+        </div>
+        <div class="col-6 col-md-4  text-left text-md-center">
+            <h4>Helpful Resources</h4>
+            <ul class="list-unstyled">
+
+            </ul>
+        </div>
+    </div>
+    <div class="row text-center text-md-left"><p class="col-12 font-size-small text-black-50">Copyright © 2019 | Takeaway</p></div>
+</div>
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,5 +1,46 @@
 @extends('layouts.home')
 
 @section('content')
+    <!-- showcase start -->
+    <section id="showcase" class="showcase">
+        <div class="container">
+            <div class="media">
+                <div class="media-body align-self-center text-left">
+                    <h2 class="mt-0">Transactional email microservice - code challenge</h2>
+                    <p>This microservice will use external services to actually sent the emails.</p>
+                </div>
+                <img class="align-self-center ml-5" src="/images/showcase-1.png" alt="Generic placeholder image">
+            </div>
+        </div>
+    </section>
+    <!-- showcase end -->
 
+    <!-- features start -->
+    <section id="products" class="products">
+        <div class="container">
+            <div class="row">
+                <div class="col-12">
+                    <div class="media-body align-self-center text-justify">
+                        <h2 class="">Service Sections</h2>
+                        <p class="lead"></p>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="card-deck">
+                    <div class="card">
+                        <div class="col-auto pt-3"></div>
+                        <div class="card-body">
+                            <h5 class="card-title text-center">Welcome Page</h5>
+                            <p class="card-text"></p>
+                        </div>
+                        <div class="card-footer text-center">
+                            <small class="text-muted"><a href="/">Welcome</a></small>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section>
+    <!-- features end -->
 @endsection
